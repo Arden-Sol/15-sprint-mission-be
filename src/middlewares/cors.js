1d1ee42f91b88f45d5fa02f2dda53daaa2b7627d
ha kyeong <hess93hess93@gmail.com>
2026-09-05 23:38:55 +0900

    fix: CORS whiteList를 배열로 변경하고 불필요한 슬래시 제거

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -2,8 +2,8 @@ import { isDevelopment, isProduction } from '../config/config.js';
 
 export const cors = (req, res, next) => {
   const whiteList = isDevelopment
-    ? 'http://localhost:5173'
-    : 'https://sprint-fullstack-15-kimhakyeong.netlify.app/';
+    ? ['http://localhost:5173']
+    : ['https://sprint-fullstack-15-kimhakyeong.netlify.app'];
 
   const origin = req.get('Origin');
 
